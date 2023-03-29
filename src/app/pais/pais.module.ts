30447183220e8ca3225541bbc4c3eb7c0e0c9af9
Edison Aguirre Sossa <eaguirresossa@gmail.com>
2023-03-29 16:48:18 -0500

    Pages and export SidebarComponent

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    PorRegionComponent,
+    VerPaisComponent
+  ],
   imports: [
     CommonModule
   ]
