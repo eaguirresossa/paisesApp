790a7a6aa5131d0f1471d9cd976a197449c02646
Edison Aguirre Sossa <eaguirresossa@gmail.com>
2023-03-30 17:00:39 -0500

    Service httpParams

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/paises.interface';
@@ -29,8 +29,15 @@ export class PaisService {
     return this.http.get<Country[]>(url);
   }
 
-  buscarRegion( termino: string): Observable<Country[]>{
-    const url = `${ this.apiURL }/region/${termino}`;
+  buscarRegion( region: string): Observable<Country[]>{
+
+    // const params = new HttpParams()
+    //   .set( 'fields', 'name,capital,flag,cca2;population' );
+
+    const url = `${ this.apiURL }/region/${region}`;
+
+    //Con esta línea podemos filtrar el campos a buscar
+    //return this.http.get<Country[]>(url, { params: params });
     return this.http.get<Country[]>(url);
   }
 }
